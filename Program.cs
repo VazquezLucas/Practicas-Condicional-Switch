5a5072baa9a193cb0aaa1b7a339aa8d4b5b32252
Lucas Vazquez <vazquezlucasmatias2001@gmail.com>
2023-02-24 17:13:40 -0300

    Practicas Condicional Switch
    Condicional Switch finalizado

--- Program.cs
+++ Program.cs
@@ -1,1 +1,122 @@
-﻿
+﻿/* 
+ //Sintaxis Switch
+using System.Linq.Expressions;
+
+switch (expresión de control){
+    case expresión constante:
+       código a realizar
+       break;
+   case expresión constante:
+       código a realizar
+       break;
+    .....
+   default
+        código a realizar
+        break;
+}
+*/
+
+using System;
+
+namespace PracticasCondicionalSwitch
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            
+            Console.WriteLine("Elige medio de transporte (coche, tren, avión)");
+
+            string medioTransporte = Console.ReadLine();
+
+            switch (medioTransporte)
+            {
+
+                case "coche":
+                    Console.WriteLine("Velocidad media: 100 km/h");
+                    break;
+
+                case "tren":
+                    Console.WriteLine("Velocidad media: 250 km/h");
+                    break;
+
+                case "avión":
+                    Console.WriteLine("Velocidad media: 800 km/h");
+                    break;
+
+                default:
+                    Console.WriteLine("Transporte no contemplado");
+                    break;
+
+            }
+
+            Console.WriteLine("Ha terminado el programa");
+
+            
+
+            Console.WriteLine("Introduce n° de mes para cálculo de la comisión");
+
+            int nMes=Int32.Parse(Console.ReadLine());
+
+            switch (nMes)
+            {
+                case 1:
+                    Console.WriteLine("Mes escogido Enero");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Mes escogido Febrero");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Mes escogido Marzo");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Mes escogido Abril");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Mes escogido Mayo");
+                    break;
+
+                case 6:
+                    Console.WriteLine("Mes escogido Junio");
+                    break;
+
+                case 7:
+                    Console.WriteLine("Mes escogido Julio");
+                    break;
+
+                case 8:
+                    Console.WriteLine("Mes escogido Agosto");
+                    break;
+
+                case 9:
+                    Console.WriteLine("Mes escogido Septiembre");
+                    break;
+
+                case 10:
+                    Console.WriteLine("Mes escogido Octubre");
+                    break;
+
+                case 11:
+                    Console.WriteLine("Mes escogido Noviembre");
+                    break;
+
+                case 12:
+                    Console.WriteLine("Mes escogido Diciembre");
+                    break;
+
+                default:
+                    Console.WriteLine("Mes incorrecto");
+                    break;
+            }
+
+
+            Console.WriteLine("Ha terminado el programa");
+            
+
+        }
+    }
+}
